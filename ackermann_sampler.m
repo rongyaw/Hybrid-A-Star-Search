e49e41f8149ea260bd815cdfb6c1fbbeb6a2b133
rongyaw <51725793+rongyaw@users.noreply.github.com>
2020-04-22 21:48:35 -0400

    Add files via upload

--- ackermann_sampler.m
+++ ackermann_sampler.m
@@ -1,4 +1,4 @@
-function goal = ackermann_sampler(source, steering, arc_length, collision_check, obstacles, id)
+function goal = ackermann_sampler(source, steering, arc_length, collision_check, obstacles, vertex, id)
 % This function returns the sampler from ackermann steering
 % The return format is [x,y,yaw,distance_travelled,mother_id]
 % 1. Setup the initial state of the sampler
@@ -7,13 +7,14 @@ x = source(1);
 y = source(2);
 yaw = source(3);
 m_distance = source(4);
-mother_id = source(5);
+%mother_id = source(5);
 current_id = source(6);
 data_x = zeros(length(steering), length(arc_length));
 data_y = data_x;
 car_length = 0.33;
 goal = [];
-arc = linspace(0.01,arc_length,6);
+arc = linspace(0.01,arc_length,5);
+open_vertex = vertex;
 % 2. Search all the possible solution and
 for i = 1:1:length(steering)
     collision = false;
@@ -36,10 +37,22 @@ for i = 1:1:length(steering)
     end
     if collision
         continue
+    % Check if the vertex has been visited
     else
-        plot(x_glob, y_glob,'-g');hold on
-        id = id + 1;
-        goal = [goal;[x_glob(end), y_glob(end), yaw_glob, m_distance + arc_length,current_id,id]];
+        if length(vertex) ~=0
+            open_dist = abs(open_vertex - [x_glob(end), y_glob(end)]);
+            open_dist_abs = open_dist(:,1) + open_dist(:,2);
+            min_open_dist_abs = min(open_dist_abs);
+        else
+            min_open_dist_abs = Inf;
+        end
+        if min_open_dist_abs < 0.5
+            continue
+        else
+            plot(x_glob, y_glob,'-b');hold on
+            id = id + 1;
+            goal = [goal;[x_glob(end), y_glob(end), yaw_glob, m_distance + arc_length,current_id,id]];
+        end
     end
 end
 end
--- hybrid_A_star.m
+++ hybrid_A_star.m
@@ -13,12 +13,19 @@ clear all
 clc
 close all
 dbstop if error
+
 %%  Create the obstacles and start/goal location
 obstacles = [1,2,0,10;
             4,5,-7,4;
-            -5,-3,-10,-2;
+            -4,-3,-10,-2;
             7,8,2,8;
-            -1,0,-2,3];% Format -> [min_x, max_x, min_y, max_y]
+            -1,0,-5,1;
+            -4,-3,0,8;
+            -2,-1,0,1;
+            3,6,4,5;
+            1,3,-1,0;
+            1,2,-7,-3;
+            4,5,7,10];% Format -> [min_x, max_x, min_y, max_y]
 
 for i = 1:1:length(obstacles(:,1))
     min_x = obstacles(i,1);
@@ -36,30 +43,36 @@ end
 
 % Setup the start and goal location for nvaigation
 start_x = -7;
-start_y = -6;
+start_y = -3;
 start_yaw = 0;
 goal_x = 10;
-goal_y = 2;
-plot(start_x, start_y, '>b', 'MarkerSize', 15, 'MarkerFaceColor', 'b');hold on
+goal_y = 1;
+plot(start_x, start_y, '>g', 'MarkerSize', 15, 'MarkerFaceColor', 'g');hold on
 plot(goal_x, goal_y, '>r', 'MarkerSize', 15, 'MarkerFaceColor', 'r');hold on
+
 %%  Create open_list and closed list
 open = [];
 open_f = [];
 open_g = [];
 open_c = [];
 close = [];
+
 %%  Create the steering angle and arc length for sampling
-steering = linspace(-0.4,0.4,8);
-arc_length = 1;
+steering = linspace(-0.4,0.4,10);
+arc_length = input('Please define sampling distance: ');
 %%  Searching over the map to reach the goal from the start
 % Initialize the open list
 % Global id keep track of the total vertex visited
 global id
 id = 1;
 mother_id = 0;
+w_gn = input('Please define the weight of g(n): ');
 open = [start_x, start_y, start_yaw, 0, mother_id, id];
+vertex_sum = [start_x, start_y];
 open_f = [open_f, pdist([open(1:2);[goal_x, goal_y]])]; % eucliden heuristic function
 open_c = open_f + open(end);
+% Start the timer
+tic
 while length(open_c) ~= 0
     % Pop the minimum cost value
     [~,source_ind] = min(open_c);
@@ -71,16 +84,21 @@ while length(open_c) ~= 0
     open(source_ind,:) = []; % Delete the pop-up key from open list
     open_f(source_ind) = []; % Same as above
     open_c(source_ind) = []; % Same as above
-    sample = ackermann_sampler(source, steering, arc_length, @collision_check, obstacles, id); % Search from the pop-up point
-    if length(sample) > 0
+    sample = ackermann_sampler(source, steering, arc_length, @collision_check, obstacles, vertex_sum, id); % Search from the pop-up point
+    if ~isempty(sample)
         f = sample(:,1:2) - [goal_x, goal_y];
         f = (f(:,1).^2 + f(:,2).^2).^0.5;
         open = [open; sample];
+        vertex_sum = [vertex_sum; sample(:,1:2)];
         open_f = [open_f, f.'];
-        open_c = open_f + 0.3*open(:,4).';
+        open_c = open_f + w_gn*open(:,4).';
+        drawnow
         % In this case, the weight of distance travelled is set to be lower than distance to the goal
     end
 end
+toc % Stop the timer
+disp(['Total node explored is ',num2str(length(vertex_sum(:,1)))]);
+
 %%  Search through the closed list to find the path.
 % Start from the last point and draw the path between them
 search_id =  close(end, 5);
@@ -91,6 +109,7 @@ while search_id ~= 0
     plot(close(point_id,1), close(point_id,2),'rs','MarkerSize',6,'MarkerFaceColor','r');hold on
     search_id = close(point_id,5);
 end
+disp(['Total distance travelled is ',num2str(length(path_point(:,1))*arc_length + arc_length)]);
 
 %% Draw the path line
 for i = length(path_point):-1:2
@@ -109,5 +128,5 @@ for i = length(path_point):-1:2
     plot(x_glob, y_glob,'-r','LineWidth',3);hold on
 end
 plot([path_point(1,1),goal_x],[path_point(1,2),goal_y],'-r','LineWidth',3);hold off
-title('Hybrid A* Search Demo','FontSize',15);
-xlim([-8,12]);grid on
+%title('Hybrid A* Search Demo','FontSize',15);
+xlim([-8,12]);
