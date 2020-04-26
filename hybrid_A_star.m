e9ce2eb90122b6c84a0b5a7dfd9e4ffa93862ae3
Rongyao Wang <rywang5235@gmail.com>
2020-04-25 23:03:06 -0400

    second commit

--- hybrid_A_star.m
+++ hybrid_A_star.m
@@ -26,7 +26,7 @@ obstacles = [1,2,0,10;
             1,3,-1,0;
             1,2,-7,-3;
             4,5,7,10];% Format -> [min_x, max_x, min_y, max_y]
-
+% Plot the obstacles in map
 for i = 1:1:length(obstacles(:,1))
     min_x = obstacles(i,1);
     max_x = obstacles(i,2);
@@ -41,7 +41,7 @@ for i = 1:1:length(obstacles(:,1))
     clear max_y;
 end
 
-% Setup the start and goal location for nvaigation
+% Setup the start and goal location for nvaigation and plot them
 start_x = -7;
 start_y = -3;
 start_yaw = 0;
@@ -51,18 +51,21 @@ plot(start_x, start_y, '>g', 'MarkerSize', 15, 'MarkerFaceColor', 'g');hold on
 plot(goal_x, goal_y, '>r', 'MarkerSize', 15, 'MarkerFaceColor', 'r');hold on
 
 %%  Create open_list and closed list
-open = [];
-open_f = [];
-open_g = [];
-open_c = [];
-close = [];
+% This is the simplified version a heap structure.
+open = []; % Store the information of vertex
+open_f = []; % Store the corresponding key values: f(n)
+open_g = []; % Store the corresponding key values: g(n)
+open_c = []; % Combine the previous two as cost value
+close = []; % Create closed list for finding optimal path
 
 %%  Create the steering angle and arc length for sampling
 steering = linspace(-0.4,0.4,10);
 arc_length = input('Please define sampling distance: ');
+
 %%  Searching over the map to reach the goal from the start
 % Initialize the open list
-% Global id keep track of the total vertex visited
+% Global id keep track of the total vertex visited, it serve the function
+% as counter
 global id
 id = 1;
 mother_id = 0;
@@ -71,6 +74,7 @@ open = [start_x, start_y, start_yaw, 0, mother_id, id];
 vertex_sum = [start_x, start_y];
 open_f = [open_f, pdist([open(1:2);[goal_x, goal_y]])]; % eucliden heuristic function
 open_c = open_f + open(end);
+
 % Start the timer
 tic
 while length(open_c) ~= 0
@@ -102,7 +106,7 @@ disp(['Total node explored is ',num2str(length(vertex_sum(:,1)))]);
 %%  Search through the closed list to find the path.
 % Start from the last point and draw the path between them
 search_id =  close(end, 5);
-path_point = [];
+path_point = []; % Store the path from closed list
 while search_id ~= 0
     point_id = find(close(:,6)== search_id);
     path_point = [path_point;[close(point_id,1), close(point_id,2), close(point_id,3)]];
@@ -128,5 +132,5 @@ for i = length(path_point):-1:2
     plot(x_glob, y_glob,'-r','LineWidth',3);hold on
 end
 plot([path_point(1,1),goal_x],[path_point(1,2),goal_y],'-r','LineWidth',3);hold off
-%title('Hybrid A* Search Demo','FontSize',15);
+title('Hybrid A* Search Demo','FontSize',15);
 xlim([-8,12]);
